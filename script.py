f7faf0f5e3df490ac63ef3e290c4c493e899e178
Chux <chuokonji299@gmail.com>
2022-06-26 19:35:58 +0100

    Modify further the generator function so that the function can take in a value using send(). The for loop was refactored so that it prints the first ten names from the guest_list text file, and then the name given with send(), after which the remaining names in guest_list is printed.

--- script.py
+++ script.py
@@ -4,8 +4,12 @@
 guests = {}
 def read_guestlist(file_name):
   text_file = open(file_name,'r')
+  n = None
   while True:
     line_data = text_file.readline().strip().split(",")
+    #enable the generator function take in a value using send():
+    if n is not None:
+        line_data = n.strip().split(',')
     if len(line_data) < 2:
     # If no more lines, close file
       text_file.close()
@@ -13,13 +17,14 @@ def read_guestlist(file_name):
     name = line_data[0]
     age = int(line_data[1])
     guests[name] = age
-    yield name
+    n = yield name
+
 
 guest_name = read_guestlist('guest_list.txt')
 
 count = 0
 for name  in guest_name:
     count += 1
-    if count == 10:
-        guest_name.close()
     print(name)
+    if count == 10:
+        print(guest_name.send('Jane, 35'))
