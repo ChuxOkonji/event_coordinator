75ed1eb5803be44f54c5f42d5f477313aaa61224
Chux <chuokonji299@gmail.com>
2022-06-26 20:45:32 +0100

    Add a generator expression that uses the guests dictionary to retrieve a generator of names of guests who are 21 and over

--- script.py
+++ script.py
@@ -4,10 +4,11 @@
 guests = {}
 def read_guestlist(file_name):
   text_file = open(file_name,'r')
+  #twe define n so that it is not undefined when the whilw loop runs
   n = None
   while True:
     line_data = text_file.readline().strip().split(",")
-    #enable the generator function take in a value using send():
+    #n is not none if a value has been taken in by the generator using send()
     if n is not None:
         line_data = n.strip().split(',')
     if len(line_data) < 2:
@@ -17,6 +18,7 @@ def read_guestlist(file_name):
     name = line_data[0]
     age = int(line_data[1])
     guests[name] = age
+    #enable the generator function take in a value using send():
     n = yield name
 
 
@@ -26,5 +28,13 @@ count = 0
 for name  in guest_name:
     count += 1
     print(name)
+    #After printing the first 10 names, print the value given via send()
     if count == 10:
         print(guest_name.send('Jane, 35'))
+
+# Define a generator expression that uses the guest dictionary to retrieve a genarator of names of guests
+# who are 21 and over
+guest_21_and_over = (name for name in guests if guests[name] >= 21)
+
+for name in guest_21_and_over:
+    print(name)
